e64be70584502c1c0ee9296bf2759abf4a2efccc
jordyf15 <jordyferdian88@gmail.com>
2022-10-09 09:00:50 +0700

    added i18n for footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,6 +12,12 @@ const Footer = () => {
       textAlign="center"
       mb="15px"
       lineHeight="unset"
+      sx={{
+        px: {
+          xs: "20px",
+          sm: "0",
+        },
+      }}
     >
       © 2022 Jordy Ferdian{" "}
       <Circle sx={{ fontSize: "4px", position: "relative", bottom: "3px" }} />{" "}
