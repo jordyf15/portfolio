5710e0a7e69aaafc556c249af1719d521d8ebfbe
jordyf15 <jordyferdian88@gmail.com>
2024-06-17 17:42:25 +0700

    added express, laravel, mysql, php, rails, and sql server to skills section

--- src/datas/skillData.ts
+++ src/datas/skillData.ts
@@ -1,4 +1,5 @@
 import cssIcon from "../assets/skill-icons/css.png";
+import expressIcon from "../assets/skill-icons/express.png";
 import firebaseIcon from "../assets/skill-icons/firebase.png";
 import ginIcon from "../assets/skill-icons/gin.png";
 import golangIcon from "../assets/skill-icons/golang.png";
@@ -6,11 +7,16 @@ import htmlIcon from "../assets/skill-icons/html.png";
 import javaIcon from "../assets/skill-icons/java.png";
 import jsIcon from "../assets/skill-icons/js.png";
 import kotlinIcon from "../assets/skill-icons/kotlin.png";
+import laravelIcon from "../assets/skill-icons/laravel.png";
 import mongoDbIcon from "../assets/skill-icons/mongodb.png";
+import mysqlIcon from "../assets/skill-icons/mysql.png";
+import phpIcon from "../assets/skill-icons/php.png";
 import postgreSqlIcon from "../assets/skill-icons/postgresql.png";
+import railsIcon from "../assets/skill-icons/rails.png";
 import reactIcon from "../assets/skill-icons/react.png";
 import redisIcon from "../assets/skill-icons/redis.png";
 import rubyIcon from "../assets/skill-icons/ruby.png";
+import sqlServerIcon from "../assets/skill-icons/sql-server.png";
 import typescriptIcon from "../assets/skill-icons/typescript.png";
 import Skill from "../models/Skill";
 
@@ -57,31 +63,61 @@ export const skills: Array<Skill> = [
   },
   {
     id: "skill-9",
+    name: "PHP",
+    iconUrl: phpIcon,
+  },
+  {
+    id: "skill-10",
     name: "React",
     iconUrl: reactIcon,
   },
   {
-    id: "skill-10",
+    id: "skill-11",
     name: "Gin",
     iconUrl: ginIcon,
   },
   {
-    id: "skill-11",
+    id: "skill-12",
+    name: "Ruby on Rails",
+    iconUrl: railsIcon,
+  },
+  {
+    id: "skill-13",
+    name: "Express",
+    iconUrl: expressIcon,
+  },
+  {
+    id: "skill-14",
+    name: "Laravel",
+    iconUrl: laravelIcon,
+  },
+  {
+    id: "skill-15",
     name: "Firebase",
     iconUrl: firebaseIcon,
   },
   {
-    id: "skill-12",
+    id: "skill-16",
     name: "MongoDB",
     iconUrl: mongoDbIcon,
   },
   {
-    id: "skill-13",
+    id: "skill-17",
     name: "PosgreSQL",
     iconUrl: postgreSqlIcon,
   },
   {
-    id: "skill-14",
+    id: "skill-18",
+    name: "MySQL",
+    iconUrl: mysqlIcon,
+  },
+  {
+    id: "skill-19",
+    name: "SQL Server",
+    iconUrl: sqlServerIcon,
+  },
+  {
+    id: "skill-20",
     name: "Redis",
     iconUrl: redisIcon,
   },
